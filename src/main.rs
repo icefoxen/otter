cdb3c576f5a9d7278653d66d15168f92938187e3
Simon Heath <icefoxen@gmail.com>
2016-11-06 17:44:31 -0500

    You know what let's try Iron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[macro_use]
+extern crate nickel;
+
+use nickel::{Nickel, HttpRouter};
+
+
+fn get_page(request: &mut nickel::Request) -> String {
+
+    "Get page!".to_owned()
+}
+
+
+fn post_page(request: &mut nickel::Request) -> String {
+
+    "Post page!".to_owned()
+}
+
+// fn test(request: &mut nickel::Request) -> String {
+//     "Test string!".to_owned()
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+    // server.utilize(nickel::StaticFilesHandler::new("htdocs"));
+    server.get("/*", middleware!(|request| get_page(request).as_str()))
+          .post("/*", middleware!(|request| post_page(request).as_str()));
+    server.listen("127.0.0.1:8080").unwrap();
 }
