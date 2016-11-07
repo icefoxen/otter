de49abb0ac625ab77e015364af66174d33d485c6
Simon Heath <icefoxen@gmail.com>
2016-11-06 19:15:26 -0500

    Basic static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,61 @@
 extern crate iron;
 extern crate router;
-extern crate staticfile;
 
+use std::fs;
+use std::io;
+use std::io::Read;
 use std::path::Path;
 
 use iron::prelude::*;
 use iron::status;
 
 use router::Router;
-use staticfile::Static;
 
+static REPO_PATH: &'static str = "pages/";
+static STATIC_PATH: &'static str = "htdocs/";
+static SERVER_ADDRESS: &'static str = "localhost:8080";
 
-fn handler(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello World!")))
-}
-
-
-fn query_handler(req: &mut Request) -> IronResult<Response> {
+fn get_page(req: &mut Request) -> IronResult<Response> {
     let ref query = req.extensions
                        .get::<Router>()
                        .unwrap()
-                       .find("query")
+                       .find("page")
                        .unwrap_or("no query");
-    let res = format!("Query is: {}", query);
+    let res = format!("Page is: {}", query);
     Ok(Response::with((status::Ok, res)))
 }
 
-fn main() {
 
+fn get_static(req: &mut Request) -> IronResult<Response> {
+    let query = req.extensions
+                   .get::<Router>()
+                   .unwrap()
+                   .find("item")
+                   .unwrap_or("dne");
+    let mut staticpath = STATIC_PATH.to_owned();
+    staticpath += query;
+    let path = Path::new(&staticpath);
+    match fs::File::open(path) {
+        Ok(file) => {
+            // TODO: Detect content-type
+            Ok(Response::with((status::Ok, file)))
+        }
+        Err(e) => {
+            let status = match e.kind() {
+                io::ErrorKind::NotFound => status::NotFound,
+                io::ErrorKind::PermissionDenied => status::Forbidden,
+                _ => status::InternalServerError,
+            };
+
+            Err(IronError::new(e, status))
+        }
+    }
+}
+
+fn main() {
     let mut router = Router::new();
-    router.get("/", handler, "index");
-    router.get("/:query", query_handler, "query");
-    router.get("/static/",
-               Static::new(Path::new("htdocs/index.html")),
-               "static");
-    let _server = Iron::new(router).http("localhost:8080").unwrap();
+    router.get("/:page", get_page, "page");
+    router.get("/static/:item", get_static, "static");
+    let _server = Iron::new(router).http(SERVER_ADDRESS).unwrap();
+    println!("Server running on {}", SERVER_ADDRESS);
 }
