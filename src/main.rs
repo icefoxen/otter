eb6fbc925e814f87888f08e051744e24cac6e596
Simon Heath <icefoxen@gmail.com>
2016-11-06 19:47:15 -0500

    Wow this whole 'middleware' thing is a pain in the butt.
    Working on being able to load template files, at least.  Blah.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate iron;
 extern crate router;
+extern crate git2;
 
 use std::fs;
 use std::io;
@@ -13,6 +14,7 @@ use router::Router;
 
 static REPO_PATH: &'static str = "pages/";
 static STATIC_PATH: &'static str = "htdocs/";
+static TEMPLATE_PATH: &'static str = "templates/";
 static SERVER_ADDRESS: &'static str = "localhost:8080";
 
 fn get_page(req: &mut Request) -> IronResult<Response> {
@@ -21,7 +23,25 @@ fn get_page(req: &mut Request) -> IronResult<Response> {
                        .unwrap()
                        .find("page")
                        .unwrap_or("no query");
+
+    // let tmpl =
     let res = format!("Page is: {}", query);
+    Ok(Response::with((status::Ok, res)))
+}
+
+fn post_page(req: &mut Request) -> IronResult<Response> {
+    let ref query = req.extensions
+                       .get::<Router>()
+                       .unwrap()
+                       .find("page")
+                       .unwrap_or("no query");
+    match git2::Repository::open(REPO_PATH) {
+        Ok(repo) => {}
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
+    let res = format!("Posted page: {}", query);
     Ok(Response::with((status::Ok, res)))
 }
 
@@ -34,8 +54,7 @@ fn get_static(req: &mut Request) -> IronResult<Response> {
                    .unwrap_or("dne");
     let mut staticpath = STATIC_PATH.to_owned();
     staticpath += query;
-    let path = Path::new(&staticpath);
-    match fs::File::open(path) {
+    match fs::File::open(staticpath) {
         Ok(file) => {
             // TODO: Detect content-type
             Ok(Response::with((status::Ok, file)))
@@ -55,6 +74,7 @@ fn get_static(req: &mut Request) -> IronResult<Response> {
 fn main() {
     let mut router = Router::new();
     router.get("/:page", get_page, "page");
+    router.post("/:page", post_page, "page");
     router.get("/static/:item", get_static, "static");
     let _server = Iron::new(router).http(SERVER_ADDRESS).unwrap();
     println!("Server running on {}", SERVER_ADDRESS);
