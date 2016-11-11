797f28e959b01b8bc4ffe71ef468df91edf14fa0
Simon Heath <icefoxen@gmail.com>
2016-11-11 12:33:08 -0500

    omg pencil is so much nicer.

--- src/main.rs
+++ src/main.rs
@@ -1,161 +1,33 @@
-extern crate iron;
-extern crate router;
-extern crate git2;
-extern crate logger;
+extern crate pencil;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-extern crate hoedown;
-extern crate handlebars_iron;
-extern crate rustc_serialize;
 
-use std::error::Error;
-use std::fs;
-use std::io;
+use std::collections::BTreeMap;
 
-use iron::prelude::*;
-use iron::status;
+use pencil::{Pencil, Request, Response, PencilResult};
 
-use router::Router;
-use logger::Logger;
-
-use rustc_serialize::json::ToJson;
-
-use hoedown::Render;
-
-static REPO_PATH: &'static str = "pages/";
-static STATIC_PATH: &'static str = "htdocs/";
-static TEMPLATE_PATH: &'static str = "templates/";
-static SERVER_ADDRESS: &'static str = "localhost:8080";
-
-
-#[derive(RustcDecodable, RustcEncodable)]
-struct PageInfo {
-    body: String,
+fn page_get(_: &mut Request) -> PencilResult {
+    Ok(Response::from("Hello World!"))
 }
 
-impl PageInfo {
-    fn new(body: &str) -> PageInfo {
-        PageInfo { body: body.to_owned() }
-    }
+fn page_post(_: &mut Request) -> PencilResult {
+    Ok(Response::from("Posted page!"))
 }
 
-// impl ToJson for PageInfo {
-//     fn to_json(&self) -> rustc_serialize::json::Json {}
-// }
-
-fn get_page(req: &mut Request) -> IronResult<Response> {
-    let ref pagename = req.extensions
-                          .get::<Router>()
-                          .unwrap()
-                          .find("page")
-                          .unwrap_or("no query");
-
-    let mut pagepath = REPO_PATH.to_owned();
-    pagepath += pagename;
-    pagepath += ".md";
-
-    match fs::File::open(pagepath) {
-        Ok(file) => {
-            let md = hoedown::Markdown::read_from(file);
-
-            let mut html = hoedown::Html::new(hoedown::renderer::html::Flags::empty(), 0);
-            let buffer = html.render(&md);
-            // This is bugged!
-            // See https://github.com/iron/iron/issues/498
-            // let br: BodyReader<hoedown::Buffer> = BodyReader(buffer);
-            // Ok(Response::with((status::Ok, br)))
-            // TODO: Set content-type
-            let stringggggg = buffer.to_str().unwrap();
-            let pageinfo = PageInfo::new(stringggggg);
-            let json = rustc_serialize::json::encode(&pageinfo).unwrap();
-            println!("JSON is: {}", json);
-            // Ok(Response::with((status::Ok, stringggggg)));
-
-            let t = handlebars_iron::Template::new("page", json);
-
-            Ok(Response::with((status::Ok, t)))
-        }
-        Err(e) => {
-            let status = match e.kind() {
-                io::ErrorKind::NotFound => status::NotFound,
-                io::ErrorKind::PermissionDenied => status::Forbidden,
-                _ => status::InternalServerError,
-            };
-
-            Err(IronError::new(e, status))
-        }
-    }
+fn test_template_get(request: &mut Request) -> PencilResult {
+    let mut ctx = BTreeMap::new();
+    ctx.insert("name".to_string(), "template".to_string());
+    return request.app.render_template("hello.html", &ctx);
 }
-
-fn post_page(req: &mut Request) -> IronResult<Response> {
-    let ref query = req.extensions
-                       .get::<Router>()
-                       .unwrap()
-                       .find("page")
-                       .unwrap_or("no query");
-    match git2::Repository::open(REPO_PATH) {
-        Ok(repo) => {}
-        Err(e) => {
-            println!("Error: {:?}", e);
-        }
-    }
-    let res = format!("Posted page: {}", query);
-    Ok(Response::with((status::Ok, res)))
-}
-
-
-fn get_static(req: &mut Request) -> IronResult<Response> {
-    let query = req.extensions
-                   .get::<Router>()
-                   .unwrap()
-                   .find("item")
-                   .unwrap_or("dne");
-    let mut staticpath = STATIC_PATH.to_owned();
-    staticpath += query;
-    match fs::File::open(staticpath) {
-        Ok(file) => {
-            // TODO: Detect content-type
-            Ok(Response::with((status::Ok, file)))
-        }
-        Err(e) => {
-            let status = match e.kind() {
-                io::ErrorKind::NotFound => status::NotFound,
-                io::ErrorKind::PermissionDenied => status::Forbidden,
-                _ => status::InternalServerError,
-            };
-
-            Err(IronError::new(e, status))
-        }
-    }
-}
-
 fn main() {
-    env_logger::init().unwrap();
-    info!("Starting...");
-    let (logger_before, logger_after) = Logger::new(None);
-
-    let mut router = Router::new();
-    router.get("/:page", get_page, "page");
-    router.post("/:page", post_page, "page");
-    router.get("/static/:item", get_static, "static");
-
-    // HandlebarsEngine will look up all files with "./examples/templates/**/*.hbs"
-    let mut hbse = handlebars_iron::HandlebarsEngine::new();
-    hbse.add(Box::new(handlebars_iron::DirectorySource::new(TEMPLATE_PATH, ".tmpl")));
-
-    // load templates from all registered sources
-    if let Err(r) = hbse.reload() {
-        panic!("{}", r.description());
-    }
-
-
-    let mut chain = Chain::new(router);
-
-    chain.link_before(logger_before);
-
-    chain.link_after(hbse);
-    chain.link_after(logger_after);
-    let _server = Iron::new(chain).http(SERVER_ADDRESS).unwrap();
-    info!("Server running on {}", SERVER_ADDRESS);
+    let mut app = Pencil::new(".");
+    app.set_debug(true);
+    app.enable_static_file_handling();
+    app.register_template("hello.html");
+    app.get("/hello_template", "hello_template", test_template_get);
+    app.get("/<page:string>", "page_get", page_get);
+    app.post("/<page:string>", "page_post", page_post);
+    debug!("* Running on http://localhost:5000/");
+    app.run("localhost:5000");
 }
